205110d888c8f520d1879d1cb0d81979a49a4c96
Ole Taege <oletaege@gmail.com>
2026-09-18 08:14:06 +0200

    Add API logging support

--- e621tags/tests/e621_api_test.php
+++ e621tags/tests/e621_api_test.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 require_once '/var/www/nextcloud/lib/base.php';
 
+use OCA\E621Tags\ApiRateLimiter;
 use OCA\E621Tags\Config;
 use OCA\E621Tags\E621Service;
 use OCP\Http\Client\IClientService;
 use OCP\IConfig;
+use Psr\Log\LoggerInterface;
 
 $config = \OC::$server->get(IConfig::class);
 $clientService = \OC::$server->get(IClientService::class);
+$rateLimiter = \OC::$server->get(ApiRateLimiter::class);
+$logger = \OC::$server->get(LoggerInterface::class);
 
 $service = new E621Service(
     $clientService,
     new Config($config),
+    $rateLimiter,
+    $logger,
 );
 
 $post = $service->getPost(1234567);
